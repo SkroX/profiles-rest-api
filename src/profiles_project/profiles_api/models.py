b1379a48a4c9dc8d599f84f4ae2d80e2fa2ef400
Shubham Kumar <shubhamcricketer.kumar27@gmail.com>
2020-02-19 19:19:21 +0530

    added profile feed item and migrated db

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -64,3 +64,14 @@ class UserProfile(AbstractBaseUser,PermissionsMixin):
         """django uses this when object needed to be converted into string"""
 
         return self.email
+
+
+class ProfileFeedItem(models.Model):
+
+    user_profile=models.ForeignKey('UserProfile',on_delete=models.CASCADE)
+    status_text=models.CharField(max_length=255)
+    created_on=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+
+        return self.status_text
--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -17,6 +17,10 @@ from rest_framework.authentication import TokenAuthentication
 
 from rest_framework import filters
 
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+
+from rest_framework.authtoken.views import ObtainAuthToken
+
 # Create your views here.
 
 class HelloApiView(APIView):
@@ -73,3 +77,12 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes=(permissions.UpdateOwnProfile,)
     filter_backends=(filters.SearchFilter,)
     search_fields=('name','email',)
+
+
+class LoginViewSet(viewsets.ViewSet):
+
+    serializer_class=AuthTokenSerializer
+
+    def create(self,request):
+
+        return ObtainAuthToken().post(request)
